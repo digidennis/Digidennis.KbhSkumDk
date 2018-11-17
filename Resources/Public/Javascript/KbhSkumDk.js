585dcbf79660078d7e3baa359da2b5fcfe92a40e
Dennis Jessen <digidennis3@gmail.com>
2018-11-17 19:57:45 +0000

    forms

--- Resources/Public/Javascript/KbhSkumDk.js
+++ Resources/Public/Javascript/KbhSkumDk.js
@@ -9,3 +9,40 @@ $(document).ready(function() {
         $(this).parent().parent().children('dd:not([class="digidennis-mageme-option-price"])').slideToggle(200,'swing');
     });
 });
+
+/*
+	By Osvaldas Valutis, www.osvaldas.info
+	Available for use under the MIT License
+*/
+
+'use strict';
+
+;( function( $, window, document, undefined )
+{
+    $( 'input[type="file"]' ).each( function()
+    {
+        var $input	 = $( this ),
+            $label	 = $input.parent().parent().prev( 'label' ),
+            labelVal = $label.html();
+
+        $input.on( 'change', function( e )
+        {
+            var fileName = '';
+
+            if( this.files && this.files.length > 1 )
+                fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+            else if( e.target.value )
+                fileName = e.target.value.split( '\\' ).pop();
+
+            if( fileName )
+                $label.html( fileName );
+            else
+                $label.html( labelVal );
+        });
+
+        // Firefox bug fix
+        $input
+            .on( 'focus', function(){ $input.addClass( 'has-focus' ); })
+            .on( 'blur', function(){ $input.removeClass( 'has-focus' ); });
+    });
+})( jQuery, window, document );
